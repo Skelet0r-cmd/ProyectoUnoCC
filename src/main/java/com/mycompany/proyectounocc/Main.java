df3cc896f3516266e64cb9d983919ff1973ec468
Carlos Castillo 1 <c.castillo15@ufromail.cl>
2020-08-02 15:41:42 -0400

    Creación de Main y Gasto

---
+++ src/main/java/com/mycompany/proyectounocc/Main.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.proyectounocc;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Addis Flerida Muñoz
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        ArrayList<Gasto> lista = new ArrayList<Gasto>();
+
+        lista.add(new Gasto("Luz", 12000));
+        lista.add(new Gasto("Agua", 7000));
+        lista.add(new Gasto("Internet", 30000));
+        lista.add(new Gasto("Gas", 20000));
+
+        double resultado = lista.stream()
+                .mapToDouble(gasto -> gasto.getValor() * 1.21)
+                .filter(gasto -> gasto < 20000)
+                .sum();
+
+        System.out.println(resultado);
+
+    }
+}
